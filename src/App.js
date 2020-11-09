bb691f2e78cc26911cde0089f269075bb6db2f6a
Weiping Sun <weipingsun@Weipings-MacBook-Pro-3.local>
2020-11-09 15:17:02 -0600

    add storage code

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify, { Auth, Storage } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isDownloaded: false};
+
+        this.downloadFile = this.downloadFile.bind(this);
+    }
+
+  downloadFile(){
+        const key = "inboundFax/2020-11-4/fax_attachment_8768251004.pdf";
+        Storage.get(key).then(function (data) {
+            console.log(`The signed URL: ${data}`);
+        }, function (err) {
+            console.error(`Got error: ${err}`);
+        });// get key from Storage.list
+
+    console.log("download file is called");
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello World!</h1>
+        <button onClick={this.downloadFile}>
+            {this.state.downloadFile ? 'YES' : 'NOT YET'}
+        </button>
       </div>
     );
   }
